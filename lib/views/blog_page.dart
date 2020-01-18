35179ecabbebff3277b5908b32b371ab8e326e5b
jweboy <1259524459@qq.com>
2020-01-18 21:51:43 +0800

    :sparkles: Add blog page

---
+++ lib/views/blog_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class BlogPage extends StatefulWidget {
+  @override
+  _BlogPageState createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  _handleItemTap(int index) {
+    print(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Flex(
+        direction: Axis.horizontal,
+        children: <Widget>[
+          Expanded(
+              flex: 1,
+              child: ListView.builder(
+                itemCount: 50,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text('如何保持学习 Node 兴趣 $index'),
+                      subtitle: Text('创建日期： 2019-07-23'),
+                      onTap: () => _handleItemTap(index),
+                    ),
+                  );
+                },
+              )),
+          Expanded(
+            flex: 2,
+            child: Container(
+                child: Container(
+              child: Center(
+                child: Text('data.'),
+              ),
+              color: Colors.tealAccent,
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/blog_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BlogDialog extends StatefulWidget {
+  @override
+  _BlogDialogState createState() => _BlogDialogState();
+}
+
+class _BlogDialogState extends State<BlogDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
